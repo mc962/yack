3aebee89c022b071568d1c2d1bbbe2805b2376b2
mc962 <michaelc962@yahoo.com>
2017-02-16 11:07:44 -0500

    Add actions to current_user_reducer

--- frontend/actions/current_user_actions.js
+++ frontend/actions/current_user_actions.js
@@ -6,6 +6,15 @@ export const RECEIVE_USER_INFORMATION = 'RECEIVE_USER_INFORMATION'
 // may want to add instnce for error checking later, what if dispatched user doesnt exist
 export const fetchUserInformation = (user) => {
   return (dispatch) => {
-    return APIUtil.fetchUserInformation(user.id).then(()=>)
+    return APIUtil.fetchUserInformation(user.id).then((userData)=> {
+      return dispatch(receiveUserInformation(userData))
+    })
+  }
+}
+
+export const receiveUserInformation = (userData) => {
+  return {
+    RECEIVE_USER_INFORMATION,
+    userData
   }
 }
