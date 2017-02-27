4370ff088b97a8336e9736859c976aa789d530e0
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-02-27 16:26:43 -0500

    Fix allowed actioncable links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'https://slacking-yack.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://slacking-yack.herokuapp.com' ]
+  # config.action_cable.url = 'https://slacking-yack.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://slacking-yack.herokuapp.com', 'http://www.slacking-yack.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
